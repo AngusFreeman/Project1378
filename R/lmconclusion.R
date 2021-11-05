fc7d2038b0e3c1cff08860ab3d87f5e7ab771c4b
Angus Freeman <angusafreeman@gmail.com>
2021-11-05 17:44:02 +1100

    yt

--- R/lmconclusion.R
+++ R/lmconclusion.R
@@ -9,7 +9,7 @@
 #' @examples lmconclusion(project)
 lmconclusion <- function(dataset) {
   if(lmfit(dataset)[4] < 0.05) {
-    paste("Conclusion: Since the p-value is less than 0.05, i.e. 0 does not lie withinn the 95% confidence interval for Beta, it is statistically unlikely that the null hypothesis is correct, so we reject the null hypothesis in favour of the alternative hypothesis and assume that Beta = ", lmfit(dataset)[4], ".", sep = "")
+    paste("Conclusion: Since the p-value is less than 0.05,  i.e. 0 does not lie withinn the 95% confidence interval for Beta, it is statistically unlikely that the null hypothesis is correct, and so We reject the null hypothesis in favour of the alternative hypothesis, and assume that Beta =", lmfit(dataset)[4], ".", sep = "n")
   } else {
     paste("Conclusion: Since the p-value is greater than 0.05, i.e. 0 lies within the 95% confidence interval for Beta, we do not have enough statisctical certainty to reject the null hypothesis in favour of the alternative hypothesis, and so we assume that Beta = 0.")
   }
